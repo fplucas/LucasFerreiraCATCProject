a5aea1eef30ec7cfcbedb49e691fca59ef39a43d
Lucas Ferreira <ferreiraplucas@gmail.com>
2025-04-29 20:07:02 +0100

    Add all commands to an array and loop through them

--- lucasFerreira.sh
+++ lucasFerreira.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-date
-hostname
-arch
-uname -a
-uptime
-whoami
-who
-w
-top
-history
+commands=(
+"date"
+"hostname"
+"arch"
+"uname -a"
+"uptime"
+"whoami"
+"who"
+"w"
+"top"
+"history"
+)
+
+for command in ${commands[@]}; do
+    echo Running $command
+    $command
+    echo
+done
