84488639d68614bc70866c114d25697320f00900
Lucas Ferreira <ferreiraplucas@gmail.com>
2025-04-29 20:26:47 +0100

    Fix the output for top - doesn't hang anymore

--- lucasFerreira.sh
+++ lucasFerreira.sh
@@ -8,12 +8,22 @@ commands=(
 "whoami"
 "who"
 "w"
-"top"
+#        top is a TUI (terminal user interface) application,
+#        therefore, it was locking my script waiting for me
+#        to interact with it. The solution I found to prevent
+#        it from happening was in the top command manual
+#        (man top)
+#        -b, --batch
+#          Starts top in Batch mode, which could be useful for sending output from top to other programs or to a file.  In this mode, top  will  not  accept
+#          input and runs until the iterations limit you've set with the ‘-n’ command-line option or until killed.
+#        -n, --iterations = NUMBER
+#          Specifies the maximum number of iterations, or frames, top should produce before ending.
+"top -bn1"
 "history"
 )
 OUTPUT_FILE='./lucasFerreira.txt'
 
-for command in ${commands[*]}; do
+for command in "${commands[@]}"; do
     echo Running $command >> $OUTPUT_FILE
     $command >> $OUTPUT_FILE
     echo >> $OUTPUT_FILE
